0f145924c362bb5267de19257233c26c398238e9
Rhunosuke <ryunoske4321@gmail.com>
2020-05-03 18:23:29 +0900

    Add: ABC165

---
+++ ABC165/a.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repI(i, d, n) for (int i = (d); i < (n); ++i)
+#define reps1(i, n) for (int i = 1; i < (n); ++i)
+using namespace std;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using ll = long long;
+using P = pair<int, int>;
+using Pl = pair<ll, ll>;
+using M = map<int, int>;
+ll const INF = 1ll<<61;
+double pi = 3.141592653589793238;
+
+int main() {
+  ll k, a, b;
+  cin >>k >> a >> b;
+  bool ans = false;
+  repI(i, a, b+1) {
+    if (i % k == 0) ans = true;
+  }
+
+  if(ans) cout << "OK" << endl;
+  else cout << "NG" << endl;
+
+
+  return 0;
+}
