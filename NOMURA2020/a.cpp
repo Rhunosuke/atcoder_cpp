d96d3025925f8bc97daa55d0e62790e8ff730b12
Rhunosuke <ryunoske4321@gmail.com>
2020-05-30 23:27:00 +0900

    Add: NOMURA2020/a, b

---
+++ NOMURA2020/a.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repI(i, d, n) for (int i = (d); i < (n); ++i)
+#define reps1(i, n) for (int i = 1; i < (n); ++i)
+using namespace std;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using ll = long long;
+using P = pair<int, int>;
+using Pl = pair<ll, ll>;
+using M = map<int, int>;
+using Ml = map<ll, ll>;
+ll const INF = 1ll<<61;
+double pi = 3.141592653589793238;
+
+int main() {
+  ll h1, m1, h2, m2, k;
+  cin >> h1 >> m1 >> h2 >> m2 >> k;
+
+  ll hm1 = 60 * h1 + m1;
+  ll hm2 = 60 * h2 + m2;
+
+  cout << hm2 - hm1 - k << endl;
+
+  return 0;
+}
