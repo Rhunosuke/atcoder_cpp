c9fab077bcde72fb12a60836c3fcb98ef1cb6e94
Rhunosuke <ryunoske4321@gmail.com>
2020-07-26 14:15:06 +0900

    Add: M-SOL2020/a~d

---
+++ M-SOL2020/b.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repI(i, d, n) for (int i = (d); i < (n); ++i)
+#define reps1(i, n) for (int i = 1; i < (n); ++i)
+using namespace std;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using ll = long long;
+using P = pair<int, int>;
+using Pl = pair<ll, ll>;
+using M = map<int, int>;
+using Ml = map<ll, ll>;
+ll const INF = 1ll<<61;
+double pi = 3.141592653589793238;
+
+int main() {
+  ll a, b, c, k;
+  cin >> a >> b >> c >> k;
+
+  ll cnt = 0;
+
+  while(b <= a) {
+    b *= 2;
+    cnt++;
+  }
+  while(c <= b) {
+    c *= 2;
+    cnt++;
+  }
+
+  if (cnt <= k) cout << "Yes" << endl;
+  else cout << "No" << endl;
+
+  return 0;
+}
