789a62d4a9d33420f80ca18993a33bda2d624e43
Rhunosuke <ryunoske4321@gmail.com>
2020-09-27 23:41:23 +0900

    Add: ACLBeginner/a~c

---
+++ ACLBeginner/c.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repI(i, d, n) for (int i = (d); i < (n); ++i)
+#define reps1(i, n) for (int i = 1; i < (n); ++i)
+using namespace std;
+using namespace atcoder;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using ll = long long;
+using P = pair<int, int>;
+using Pl = pair<ll, ll>;
+using M = map<int, int>;
+using Ml = map<ll, ll>;
+ll const INF = 1ll<<61;
+double pi = 3.141592653589793238;
+
+int main() {
+  ll n, m;
+  cin >> n >> m;
+
+  dsu d(n);
+  rep(i, m) {
+    ll a, b;
+    cin >> a >> b;
+    a--; b--;
+    d.merge(a, b);
+  }
+
+  cout << d.groups().size() - 1 << endl;
+
+  return 0;
+}
