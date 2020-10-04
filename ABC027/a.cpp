0bea210e34987727c8ff1ca4916c2037eda73fc8
Rhunosuke <ryunoske4321@gmail.com>
2020-10-05 00:31:53 +0900

    Add: ABC027/a

---
+++ ABC027/a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repI(i, d, n) for (int i = (d); i < (n); ++i)
+#define reps1(i, n) for (int i = 1; i < (n); ++i)
+using namespace std;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using ll = long long;
+using P = pair<int, int>;
+using Pl = pair<ll, ll>;
+using M = map<int, int>;
+using Ml = map<ll, ll>;
+ll const INF = 1ll<<61;
+double pi = 3.141592653589793238;
+
+int main() {
+  ll l1, l2, l3;
+  cin >> l1 >> l2 >> l3;
+
+  if (l1 == l2) cout << l3 << endl;
+  else if (l2 == l3) cout << l1 << endl;
+  else if (l3 == l1) cout << l2 << endl;
+
+  return 0;
+}
