0620a309cdf8d7743d84417698436a4e75a7d6d4
Rhunosuke <ryunoske4321@gmail.com>
2020-11-11 21:02:06 +0900

    Add: ABC180/c

---
+++ ABC180/c.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repI(i, d, n) for (int i = (d); i < (n); ++i)
+#define reps1(i, n) for (int i = 1; i < (n); ++i)
+using namespace std;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using ll = long long;
+using P = pair<int, int>;
+using Pl = pair<ll, ll>;
+using M = map<int, int>;
+using Ml = map<ll, ll>;
+ll const INF = 1ll<<61;
+double pi = 3.141592653589793238;
+
+int main() {
+  ll n;
+  cin >> n;
+  set<ll> st;
+
+  for (ll i = 1; i * i <= n; i++) {
+    if (n % i == 0) {
+      st.insert(i);
+      st.insert(n / i);
+    }
+  }
+
+  for (auto itr = st.begin(); itr != st.end(); itr++) {
+    cout << *itr << endl;
+  }
+
+  return 0;
+}
