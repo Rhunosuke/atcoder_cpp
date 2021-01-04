fec48af48f2a0ebe023cc9f588d34066a887083e
Rhunosuke <ryunoske4321@gmail.com>
2021-01-04 23:14:58 +0900

    Add: ABC187/a,b

---
+++ ABC187/a.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repI(i, d, n) for (int i = (d); i < (n); ++i)
+#define reps1(i, n) for (int i = 1; i < (n); ++i)
+using namespace std;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using ll = long long;
+using P = pair<int, int>;
+using Pl = pair<ll, ll>;
+using M = map<int, int>;
+using Ml = map<ll, ll>;
+ll const INF = 1ll<<61;
+double pi = 3.141592653589793238;
+
+int main() {
+  string a, b;
+  cin >> a >> b;
+  ll na = 0, nb = 0;
+
+  for(char c: a) na += c - '0';
+  for(char c: b) nb += c - '0';
+
+  if (na >= nb) cout << na << endl;
+  else cout << nb << endl;
+
+  return 0;
+}
---
+++ ABC187/b.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repI(i, d, n) for (int i = (d); i < (n); ++i)
+#define reps1(i, n) for (int i = 1; i < (n); ++i)
+using namespace std;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using ll = long long;
+using P = pair<int, int>;
+using Pl = pair<ll, ll>;
+using M = map<int, int>;
+using Ml = map<ll, ll>;
+ll const INF = 1ll<<61;
+double pi = 3.141592653589793238;
+
+int main() {
+  ll n;
+  cin >> n;
+  vector<ll> x(n), y(n);
+  rep(i, n) cin >> x[i] >> y[i];
+  ll ans = 0;
+
+  rep(i, n) {
+    repI(j, i+1, n) {
+      if (abs(y[j] - y[i]) <= abs(x[j] - x[i])) ans++;
+    }
+  }
+
+  cout << ans << endl;
+
+  return 0;
+}
