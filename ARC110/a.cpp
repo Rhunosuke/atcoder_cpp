c14c78bb01249247526c1eef92e26378ff1d412c
Rhunosuke <ryunoske4321@gmail.com>
2021-02-28 23:58:22 +0900

    Add: ARC110/a

---
+++ ARC110/a.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repI(i, d, n) for (int i = (d); i < (n); ++i)
+#define reps1(i, n) for (int i = 1; i < (n); ++i)
+using namespace std;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using ll = long long;
+using P = pair<int, int>;
+using Pl = pair<ll, ll>;
+using M = map<int, int>;
+using Ml = map<ll, ll>;
+ll const INF = 1ll<<61;
+double pi = 3.141592653589793238;
+
+int main() {
+  ll n;
+  cin >> n;
+  ll ans = 1;
+  ll MOD = 1e13+1+n;
+  repI(i, 2, n+1) {
+    ans *= i;
+    ans %= MOD;
+  }
+  if (ans <= n) ans += 1e13+1;
+
+  cout << ans+1 << endl;
+
+  return 0;
+}
