936b4fd50a9199d344c027299ac6e5b4b98bc2e1
Rhunosuke <ryunoske4321@gmail.com>
2021-04-27 23:49:33 +0900

    Update: ABC199/a

--- ABC199/a.cpp
+++ ABC199/a.cpp
@@ -18,7 +18,7 @@ int main() {
   ll a, b, c;
   cin >> a >> b >> c;
 
-  if (a*a + b*b < c) cout << "Yes" << endl;
+  if (a*a + b*b < c*c) cout << "Yes" << endl;
   else cout << "No" << endl;
 
   return 0;
