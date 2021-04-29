4629a7d35b1a0e14d8ec09c5214f4081c56d8f5e
Rhunosuke <ryunoske4321@gmail.com>
2021-04-29 23:14:58 +0900

    Add: ABC199/b

---
+++ ABC199/b.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define repI(i, d, n) for (int i = (d); i < (n); ++i)
+#define reps1(i, n) for (int i = 1; i < (n); ++i)
+using namespace std;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using ll = long long;
+using P = pair<int, int>;
+using Pl = pair<ll, ll>;
+using M = map<int, int>;
+using Ml = map<ll, ll>;
+ll const INF = 1ll<<61;
+double pi = 3.141592653589793238;
+
+int main() {
+  ll n;
+  cin >> n;
+  ll a = 0, b = 1001;
+  ll tmp;
+  rep(i, n) {
+    cin >> tmp;
+    chmax(a, tmp);
+  }
+
+  rep(i, n) {
+    cin >> tmp;
+    chmin(b, tmp);
+  }
+
+  cout << max(0ll, b-a+1) << endl;
+
+  return 0;
+}
